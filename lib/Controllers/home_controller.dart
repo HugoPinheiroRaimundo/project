21ed0d0e9d06ad903ee493eb83792876546c169a
hugo pinheiro <hugo.pinheiro@toodoo.com.br>
2022-12-16 16:21:01 -0300

    add controller home and refactor pubspec

--- lib/Controllers/home_controller.dart
+++ lib/Controllers/home_controller.dart
@@ -1,7 +1,17 @@
+import 'package:project/model/post_model.dart';
 import 'package:project/repositories/home_repository.dart';
+import 'package:flutter/material.dart';
 
 class HomeController {
   // injetando a interface do home repository
   final HomeRepository _homeRepository;
   HomeController(this._homeRepository);
+
+// criando o objeto valuenotifier
+  ValueNotifier<List<PostModel>> posts = ValueNotifier<List<PostModel>>([]);
+
+  fetch() async {
+    posts.value = await _homeRepository
+        .getList(); // o fetch vai na api pega o getlist e retorna no post.value
+  }
 }
