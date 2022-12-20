a4a4846b485b7950d3a744ce87ebae8d40940d2b
hugo pinheiro <hugo.pinheiro@toodoo.com.br>
2022-12-19 22:00:31 -0300

    Data mook and controles

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:project/Controllers/home_controller.dart';
+import 'package:project/model/post_model.dart';
+import 'package:project/repositories/home_repository_mock.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +11,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final HomeController _controller = HomeController(HomeRepositoryMock());
+
+  @override
+  void initState() {
+    super
+        .initState(); // assim que a homepage for iniciada faça alguma coisa, nesse caso usando o controller para fazer a função fetch
+    _controller.fetch();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ValueListenableBuilder<List<PostModel>>(
+        valueListenable: _controller
+            .posts, //observando a controle do post com o valuenotifierzs
+        builder: (_, list, __) {
+          return ListView.builder(
+            itemCount: list.length,
+            itemBuilder: (_, idx) => ListTile(
+              title: Text(list[idx].title),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
--- lib/repositories/home_repository_mock.dart
+++ lib/repositories/home_repository_mock.dart
@@ -2,13 +2,14 @@ import 'dart:convert';
 
 import 'package:project/model/post_model.dart';
 import 'package:project/repositories/home_repository.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/services.dart'; // habilita o rootBundle
 
 class HomeRepositoryMock implements HomeRepository {
   @override
   Future<List<PostModel>> getList() async {
     // variável para ler o json dentro de data, rootBundle identifica tudo dentro de asssets
-    var value = await rootBundle.loadString('assets/data.json');
+    var value = await rootBundle
+        .loadString('assets/data.json'); // await pois é um feature de string
     List postJson = jsonDecode(value); // lista de post json
     return postJson
         .map((e) => PostModel.fromJson(e))
